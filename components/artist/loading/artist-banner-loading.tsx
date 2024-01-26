f2b3b9a4c31f10dd296f473309931427991bc7c0
zikri124 <zikriaizet@gmail.com>
2024-01-26 17:04:28 +0700

    fix: adjust the loader skeleton for artist banner section in artist page

--- components/artist/loading/artist-banner-loading.tsx
+++ components/artist/loading/artist-banner-loading.tsx
@@ -2,8 +2,12 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 const ArtistBannerLoading = () => {
     return (
-        <div className="w-full aspect-[4/3] relative">
-            <Skeleton className="w-full aspect-[4/3] object-cover rounded-none" />
+        <div className="w-full relative flex flex-col md:flex-row justify-items-center items-center gap-12 py-4 md:py-8 px-4">
+            <Skeleton className="w-full h-auto md:w-[22rem] aspect-square rounded-none" />
+            <div className="w-full">
+                <Skeleton className="w-1/6 h-4 mb-4" />
+                <Skeleton className="w-1/3 h-12" />
+            </div>
         </div>
     )
 }
